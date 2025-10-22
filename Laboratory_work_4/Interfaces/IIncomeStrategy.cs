df6190578331fb8fef5f10e641e782552e7739a3
Gen346 <kelzreez@gmail.com>
2025-10-22 23:59:09 +0300

    Moved fields with information about taxes into IncomeResult

---
+++ Laboratory_work_4/Interfaces/IIncomeStrategy.cs
@@ -0,0 +1,11 @@
+﻿using Laboratory_work_4.Subjects.Helper;
+
+namespace Laboratory_work_4.Interfaces
+{
+    public interface IIncomeStrategy
+    {
+        IncomeResult GetIncome(decimal currentWealth);
+        string StrategyName { get; }
+        // We no longer need IncomeType property as the tax payment logic determines status
+    }
+}
